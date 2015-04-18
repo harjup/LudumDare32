0d00d0aeb5ba96c441c64c036eccfdce97592490
kdhyne <kdhyne@gmail.com>
2015-04-18 12:34:15 -0400

    Add Civilian prefab

---
+++ src/Assets/Scripts/Civilian.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Civilian : MonoBehaviour
+    {
+        public float MoneyHeld;
+        public float MoveSpeed;
+
+        private Rigidbody2D _rigidbody2D;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
+            _rigidbody2D.velocity = new Vector2(MoveSpeed, 0);
+        }
+	
+        // Update is called once per frame
+        void Update ()
+        {
+            _rigidbody2D.velocity = new Vector2(MoveSpeed, 0);
+        }
+    }
+}
